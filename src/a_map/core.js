be685eeeeb8e8b158d9345bab7f6db4b7e80be40
«Pablo <pros7@xtec.cat>
2023-10-04 01:50:00 +0200

    filter

--- src/a_map/core.js
+++ src/a_map/core.js
@@ -1,17 +1,35 @@
 function multiplyBy10(array) {
-  // your code here
+  return array.map((element) => {
+    return element * 10;
+  });
 }
 
+//MAP NO MODIFICA L'ARRAY ORIGINAL
 function shiftRight(array) {
-  // your code here
+  return array.map((element, index, array) => {
+    // Si l'index és 0, retornem l'últim element de l'array
+    if (index === 0) {
+      return array[array.length - 1];
+    } else {
+      // Si no, retornem l'anterior
+      return array[index - 1];
+    }
+  });
 }
 
+//[^aeiou]: The ^ character inside square brackets means "not". So, [^aeiou] matches any character that is not a, e, i, o, or u.
 function onlyVowels(array) {
-  // your code here
+  return array.map((element) => {
+    return element.replace(/[^aeiou]/gi, '');
+  });
 }
 
 function doubleMatrix(array) {
-  // your code here
+  let double = function (x) {
+    return x * 2;
+  };
+  let doubleMatrix = array.map((subArray) => subArray.map(double));
+  return doubleMatrix;
 }
 
 module.exports = {
--- src/b_filter/core.js
+++ src/b_filter/core.js
@@ -1,17 +1,29 @@
+// FILTER creates a new array with all elements that pass the test implemented by the provided function.
+
 function onlyEven(array) {
-  // your code here
+  return array.filter((element) => element % 2 === 0);
 }
 
+// Farem servir RegEx
+// Si "test" el troba, retornarà booleà.
 function onlyOneWord(array) {
-  // your code here
+  return array.filter((element) => !/\s/g.test(element));
 }
+//Aquesta no cal negar-la: return array.filter(item => /^[^\s]*$/.test(item));
 
+//"every" retorna true si cada element de l'array compleix la condició.
 function positiveRowsOnly(array) {
-  // your code here
+  return array.filter((row) => row.every((number) => number > 0));
 }
 
+//1r. Creem un array amb totes les vocals de cada paraula
+//2n. Comprovem que totes les vocals siguin iguals
 function allSameVowels(array) {
-  // your code here
+  return array.filter((word) => {
+    const regexp = /[a,e,i,o,u,A,E,I,O,U]/g;
+    const vowels = [...word.matchAll(regexp)];
+    return vowels.every(([vowel], _, [firstVowel]) => vowel === firstVowel[0]);
+  });
 }
 
 module.exports = {
